959d37a57146a7a6fb6de23380276e54b1ab7599
Sahil Singh <akasahilsingh@gmail.com>
2025-11-05 17:10:31 +0530

    feat: completed admin login page

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import AdminContextProvider from "./Context/adminContext.jsx";
+import DoctorContextProvider from "./Context/Doctorcontext.jsx";
+import AppContextProvider from "./Context/AppContext.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <AdminContextProvider>
+      <DoctorContextProvider>
+        <AppContextProvider>
+          <App />
+        </AppContextProvider>
+      </DoctorContextProvider>
+    </AdminContextProvider>
+  </BrowserRouter>
+);
