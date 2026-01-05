ef927f5577a386d350bc108893c3578c6b945ba9
Sahil Singh <akasahilsingh@gmail.com>
2026-01-05 14:36:41 +0530

    feat: added Navbar to Admin panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,12 +2,20 @@ import React, { useContext } from "react";
 import Login from "./Pages/Login";
 import { ToastContainer, toast } from "react-toastify";
 import { AdminContext } from "./Context/AdminContext.jsx";
+import Navbar from "./Components/Navbar";
+import Sidebar from "./Components/Sidebar";
+
+
 
 const App = () => {
   const { atoken } = useContext(AdminContext);
   return atoken ? (
     <div className="bg-[#f8f9fd]">
       <ToastContainer />
+      <Navbar />
+      <div className="flex items-start">
+        <Sidebar />
+        </div>
     </div>
   ) : (
     <>
--- admin/src/Components/Navbar.jsx
+++ admin/src/Components/Navbar.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useContext} from 'react'
+import { assets } from '../assets/assets'
+import { AdminContext } from '../Context/AdminContext'
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+  const { atoken, setAtoken } = useContext(AdminContext)
+  const navigate = useNavigate()
+  const logout = () => {
+    navigate('/')
+    atoken && setAtoken('')
+    atoken && localStorage.removeItem('atoken')
+  }
   return (
-    <div>
-      
+    <div className="flex justify-between items-center px-4 sm:px-10 py-3 border-b bg-white">
+        <div className='flex items-center gap-2 text-xs'>
+          <img className='w-36 sm:w-40 cursor-pointer' src={assets.admin_logo} />
+          <p className='border px-2.5 py-0.5 rounded-full border-gray-500 text-gray-600'>{atoken ? "Admin" : "Doctor"}</p>
+        </div>
+        <button onClick={() => logout()} className="bg-primary text-white text-sm px-10 py-2 rounded-full">Logout</button>
+        
+
     </div>
   )
 }
