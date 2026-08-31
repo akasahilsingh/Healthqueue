16f5b9f1b52e187d1e8aba2833bac83ed59a9a67
Sahil Singh <akasahilsingh@gmail.com>
2026-08-31 09:45:11 +0530

    feat: implement getAllDoctors API and update DoctorsList component; enhance AdminContext with doctors state management

--- admin/src/Context/AdminContext.jsx
+++ admin/src/Context/AdminContext.jsx
@@ -1,20 +1,47 @@
-import { createContext } from "react";
-import { useState } from "react";
+import axios from "axios";
+import { createContext, useState } from "react";
+import { toast } from "react-toastify";
 
 export const AdminContext = createContext();
 
 const AdminContextProvider = (props) => {
-  const [atoken, setAtoken] = useState(localStorage.getItem("atoken") ? localStorage.getItem("atoken") : "");
+  const [atoken, setAtoken] = useState(
+    localStorage.getItem("atoken") ? localStorage.getItem("atoken") : "",
+  );
+  const [doctors, setDoctors] = useState([]);
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  const getAllDoctors = async () => {
+    try {
+      const { data } = await axios.post(
+        backendUrl + "/api/admin/all-doctor",
+        {},
+        { headers: { atoken } },
+      );
+
+      if (data.success) {
+        setDoctors(data.doctors);
+        console.log(data);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
+
   const value = {
     atoken,
     setAtoken,
     backendUrl,
+    doctors,
+    setDoctors,
+    getAllDoctors,
   };
+
   return (
-    <AdminContext.Provider value={value}>
-      {props.children}
-    </AdminContext.Provider>
+    <AdminContext.Provider value={value}>{props.children}</AdminContext.Provider>
   );
 };
 
--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import AdminContextProvider from "./Context/adminContext.jsx";
+import AdminContextProvider from "./Context/AdminContext.jsx";
 import DoctorContextProvider from "./Context/Doctorcontext.jsx";
 import AppContextProvider from "./Context/AppContext.jsx";
 
